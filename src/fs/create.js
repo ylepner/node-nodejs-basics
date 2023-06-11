05f6ff3f9fb76c7d614ba85e351df2778c014197
Julia Lepner <ylepner@gmail.com>
2023-06-11 22:32:17 +0200

    feat: implement function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile, stat } from "fs/promises";
+
+async function fileExists(fileName) {
+  try {
+    await stat(fileName)
+    return true
+  } catch {
+    return false
+  }
+}
 const create = async () => {
-    // Write your code here 
+  const fileName = './files/fresh.txt';
+  if (await fileExists(fileName)) {
+    throw new Error('FS operation failed')
+  }
+  await writeFile('./files/fresh.txt', 'I am fresh and young');
 };
 
 await create();
