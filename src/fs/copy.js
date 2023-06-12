e5a6a29a082085cad069430366e7eae47b8aaf66
Julia Lepner <ylepner@gmail.com>
2023-06-12 11:48:29 +0200

    feat: check if dir exists

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { stat } from 'fs/promises';
+
+async function isExists(dir) {
+  try {
+    await stat(dir)
+    return true
+  } catch {
+    return false
+  }
+}
+
+
 const copy = async () => {
-    // Write your code here 
+  const folderPath = './files';
+  const newFolderPath = './files_copy';
+
+  if (await isExists(newFolderPath)) {
+    throw new Error('FS operation failed')
+  } else {
+    console.log('Lets copy the folder!')
+  }
+
 };
 
 await copy();
