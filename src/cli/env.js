57202165e6c05a682d999f2a5c2079a68577ae32
Julia Lepner <ylepner@gmail.com>
2023-06-13 11:37:46 +0200

    feat: implement function parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,6 @@
 const parseEnv = () => {
-    // Write your code here 
+  const variables = Object.entries(process.env).filter(([variable]) => variable.startsWith('RSS_')).map((el) => `${el[0]}=${el[1]}`).join('; ')
+  console.log(variables)
 };
 
 parseEnv();
