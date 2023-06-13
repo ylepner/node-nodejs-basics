6e4a54a70f77e5b151fd3723f949698decb8663e
Julia Lepner <ylepner@gmail.com>
2023-06-13 15:35:22 +0200

    feat: implement function parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const arrayArgs = process.argv.slice(2)
+  const newArray = []
+  for (let i = 0; i < arrayArgs.length; i++) {
+    if (arrayArgs[i].startsWith('--')) {
+      const arg = `${arrayArgs[i].slice(2)} is ${arrayArgs[i + 1]}`
+      newArray.push(arg)
+    }
+  }
+  console.log(newArray.join(', '))
 };
 
 parseArgs();
