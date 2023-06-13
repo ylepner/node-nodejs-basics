fd70549f86b43003e9cdc630cff559c0c1778821
Julia Lepner <ylepner@gmail.com>
2023-06-13 09:41:17 +0200

    feat: implement function rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,13 @@
-const rename = async () => {
-    // Write your code here 
+import { rename } from 'fs/promises';
+
+const renameFile = async () => {
+  const currentFileName = './files/wrongFilename.txt';
+  const newFileName = './files/properFilename.md';
+  try {
+    await rename(currentFileName, newFileName)
+  } catch {
+    throw new Error('FS operation failed')
+  }
 };
 
-await rename();
+await renameFile();
