6691f23445a11f6c0110ab5afe820261adac1643
Julia Lepner <ylepner@gmail.com>
2023-06-14 15:13:20 +0200

    feat: implement function write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import { createWriteStream } from 'fs';
+
 const write = async () => {
-    // Write your code here 
+  const file = './files/fileToWrite.txt'
+  const stream = createWriteStream(file)
+  process.stdin.on('data', (data) => {
+    stream.write(data);
+    process.exit()
+  })
 };
 
 await write();
