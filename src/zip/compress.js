46b18cd7fd68556558b726e7073b428441c3a032
Julia Lepner <ylepner@gmail.com>
2023-06-15 22:21:14 +0200

    feat: implement function compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
-const compress = async () => {
-    // Write your code here 
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+
+const source = './files/fileToCompress.txt';
+const destination = './files/archive.gz';
+const pipe = promisify(pipeline);
+
+const compress = async (input, output) => {
+  const gzip = createGzip();
+  const source = createReadStream(input);
+  const destination = createWriteStream(output);
+  await pipe(source, gzip, destination);
 };
 
-await compress();
+await compress(source, destination);
