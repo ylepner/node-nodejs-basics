a3009cab9817b505d848cef22e1fca364f1f9d99
Julia Lepner <ylepner@gmail.com>
2023-06-15 21:58:58 +0200

    feat: implement function transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+
+const reverseTransform = new Transform({
+  transform(chunk, __, callback) {
+    const reversedChunk = chunk.toString().split('').reverse().join('');
+    callback(null, reversedChunk)
+  }
+});
+
 const transform = async () => {
-    // Write your code here 
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
-await transform();
+await transform()
