fea778e5288fb5cbd3883c55245e18cbd6471029
Julia Lepner <ylepner@gmail.com>
2023-06-15 22:53:35 +0200

    feat: add function decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
-const decompress = async () => {
-    // Write your code here 
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
+const pipe = promisify(pipeline);
+
+const source = './files/archive.gz';
+const destination = './files/fileToCompress_result.txt';
+
+const decompress = async (input, output) => {
+  const unzip = createUnzip();
+  const source = createReadStream(input);
+  const destination = createWriteStream(output);
+  await pipe(source, unzip, destination);
 };
 
-await decompress();
+await decompress(source, destination);
