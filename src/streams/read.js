72eb8eb8983d0498d51ae261c9cc7dd902975099
Julia Lepner <ylepner@gmail.com>
2023-06-15 15:43:27 +0200

    refactor: correct function read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -3,11 +3,7 @@ import { createReadStream } from 'fs';
 const read = async () => {
   const file = './files/fileToRead.txt'
   const stream = createReadStream(file);
-  const data = []
-  stream.on('data', (chunk) => {
-    data.push(chunk.toString());
-  });
-  stream.on('end', () => process.stdout.write(data.join('')));
+  stream.pipe(process.stdout)
 };
 
 await read();
