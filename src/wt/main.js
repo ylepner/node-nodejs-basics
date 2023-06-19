cd96937b9acec1c1019a6dfc152257d52644222e
Julia Lepner <ylepner@gmail.com>
2023-06-19 11:56:08 +0200

    feat: implement main

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+const filename = './worker.js'
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpusNumber = cpus().length;
+  let resultPromises = [];
+  for (let i = 0; i < cpusNumber; i++) {
+    resultPromises.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker(filename, { workerData: i + 10 });
+        worker.on('message', (message) => resolve({
+          status: 'resolved',
+          data: message
+        }));
+        worker.on('error', () => reject({
+          status: 'error',
+          data: null
+        }))
+      }))
+  }
+  Promise.all(resultPromises).then((val) => {
+    console.log(val)
+  })
 };
 
 await performCalculations();
